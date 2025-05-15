b8c55565ed9ceea92892d14bd2202d6b22418e4f
Alexandr <alexandr.zh@ideasoft.com>
2025-05-15 19:44:42 +0300

    minor changes

--- Sources/URLMacro/URLMacro.swift
+++ Sources/URLMacro/URLMacro.swift
@@ -2,16 +2,6 @@
 // https://docs.swift.org/swift-book
 
 import Foundation
-
-/// A macro that produces both a value and a string containing the
-/// source code that generated the value. For example,
-///
-///     #stringify(x + y)
-///
-/// produces a tuple `(x + y, "x + y")`.
-@freestanding(expression)
-public macro stringify<T>(_ value: T) -> (T, String) = #externalMacro(module: "URLMacroMacros", type: "StringifyMacro")
-
 /// A macro that produces an unwrapped URL in case of a valid input URL.
 /// For example,
 ///
